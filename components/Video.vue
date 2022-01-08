ee45a40bd9dcdce8d7af3b104f8eed591e6723e0
hahaha1618 <alpha.lloyd1368@gmail.com>
2022-01-08 23:05:27 +0700

    video

---
+++ components/Video.vue
@@ -0,0 +1,25 @@
+<template>
+  <vue-plyr>
+    <div class="plyr__video-embed">
+      <iframe
+        src="https://www.youtube.com/watch?v=kJQP7kiw5Fk&list=RD1ZRb1we80kM&index=2&ab_channel=LuisFonsiVEVO"
+        allowfullscreen
+        allowtransparency
+        allow="autoplay"
+      ></iframe>
+    </div>
+  </vue-plyr>
+</template>
+
+<script>
+export default {
+  name: 'Music',
+  data() {
+    return {
+      image: require('@/assets/images/datetime.jpg'),
+    }
+  },
+}
+</script>
+
+<style></style>
