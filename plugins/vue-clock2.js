dc96a65c5bad09ac27e4d733017bdb09577cac38
hahaha1618 <alpha.lloyd1368@gmail.com>
2022-01-08 23:04:25 +0700

    add plugins

---
+++ plugins/vue-clock2.js
@@ -0,0 +1,4 @@
+import Vue from "vue"
+import Clock from "vue-clock2"
+
+Vue.component("clock", Clock)
---
+++ plugins/vuetify-audio.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import VuetifyAudio from 'vuetify-audio'
+
+Vue.use(VuetifyAudio);
