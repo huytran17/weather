8b2db9c8b45ba0c6f616a3cfbe82e4c65d4381b4
hahaha1618 <alpha.lloyd1368@gmail.com>
2022-01-09 13:31:57 +0700

    country mixin

--- mixins/country.js
+++ mixins/country.js
@@ -4,13 +4,20 @@ export default {
     computed: {
         ...mapGetters({
             country_codes: "country/country_codes",
-            country_flags: "country/country_flags",
         })
     },
     methods: {
         ...mapActions({
             GET_COUNTRY_CODES: "country/GET_COUNTRY_CODES",
-            GET_COUNTRY_FLAGS: "country/GET_COUNTRY_FLAGS"
-        })
+        }),
+        /**
+         * 
+         * @param {*} country_code 
+         * @returns country flag image url
+         */
+        getCountryFlag(country_code) {
+            const flag_image_link = `https://flagcdn.com/48x36/${country_code}.png`;
+            return flag_image_link;
+        }
     }
 }
